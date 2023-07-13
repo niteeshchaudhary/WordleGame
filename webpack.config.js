9bf5df005de3e1ef4f3249091a1f36925fccb5cf
niteeshchaudhary <nkchaudhary00@gmail.com>
2023-07-14 00:06:37 +0530

    babel

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = function override(config) {
+    config.module.rules.push({
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react']
+        }
+      }
+    });
+  
+    return config;
+  };
+  
